1828c6a5cedfac06cd694c5ce19df4527cc2d6d5
uu082799 <ymotoba@gmail.com>
2020-01-23 17:25:39 +0900

    Daggerの依存追加とちょっとだけ実装

---
+++ app/src/main/java/com/example/android/githubbrowser/GithubBrowserApp.kt
@@ -0,0 +1,17 @@
+package com.example.android.githubbrowser
+
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class GithubBrowserApp : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
+    }
+}
