351feb99bfdb3623e1f42cbe69f0ee783a0b9130
uu082799 <ymotoba@gmail.com>
2020-01-27 19:21:23 +0900

    API呼び出しはrunCatchingに修正

--- app/src/main/java/com/example/android/githubbrowser/MainViewModel.kt
+++ app/src/main/java/com/example/android/githubbrowser/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.githubbrowser.repository.api.GithubApi
+import com.example.android.githubbrowser.repository.api.response.RepoSearchResponse
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -11,13 +12,24 @@ class MainViewModel @Inject constructor(private val githubApi: GithubApi) : View
 
     fun searchRepos() {
         viewModelScope.launch {
-            try {
-                Log.d("AAA", "start githubApi.searchRepos")
-                val res = githubApi.searchRepos("test")
-                Log.d("AAA", res.items[0].id.toString())
-            } catch (e: Exception) {
-                Log.e("AAA", e.message, e)
+            runCatching {
+                githubApi.searchRepos("test")
             }
+                .onSuccess { searchReposSuccess(it) }
+                .onFailure { searchReposFailure(it) }
+                .also { searchReposAlso(it) }
         }
+    }
+
+    private fun searchReposSuccess(response: RepoSearchResponse) {
+        Log.d("AAA", response.items[0].id.toString())
+    }
+
+    private fun searchReposFailure(t: Throwable) {
+        Log.e("AAA", t.message, t)
+    }
+
+    private fun searchReposAlso(result: Result<RepoSearchResponse>) {
+        Log.e("AAA", result.isFailure.toString())
     }
 }
