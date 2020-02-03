a8b64f2b9770371cac930725aa2fe43098ec58e3
uu082799 <ymotoba@gmail.com>
2020-02-03 15:22:35 +0900

    I/Fの可読性がイマイチだったのでリファクタ

--- repository/src/main/java/com/example/android/githubbrowser/repository/api/GithubApi.kt
+++ repository/src/main/java/com/example/android/githubbrowser/repository/api/GithubApi.kt
@@ -10,26 +10,44 @@ import retrofit2.http.Query
 
 interface GithubApi {
     @GET("users/{login}")
-    suspend fun getUser(@Path("login") login: String): ApiResponse<User>
+    suspend fun getUser(
+        @Path("login")
+        login: String
+    ): ApiResponse<User>
 
     @GET("users/{login}/repos")
-    suspend fun getRepos(@Path("login") login: String): ApiResponse<List<Repo>>
+    suspend fun getRepos(
+        @Path("login")
+        login: String
+    ): ApiResponse<List<Repo>>
 
     @GET("repos/{owner}/{name}")
     suspend fun getRepo(
-        @Path("owner") owner: String,
-        @Path("name") name: String
+        @Path("owner")
+        owner: String,
+        @Path("name")
+        name: String
     ): ApiResponse<Repo>
 
     @GET("repos/{owner}/{name}/contributors")
     suspend fun getContributors(
-        @Path("owner") owner: String,
-        @Path("name") name: String
+        @Path("owner")
+        owner: String,
+        @Path("name")
+        name: String
     ): ApiResponse<List<Contributor>>
 
     @GET("search/repositories")
-    suspend fun searchRepos(@Query("q") query: String): RepoSearchResponse
+    suspend fun searchRepos(
+        @Query("q")
+        query: String
+    ): RepoSearchResponse
 
     @GET("search/repositories")
-    suspend fun searchRepos(@Query("q") query: String, @Query("page") page: Int): RepoSearchResponse
+    suspend fun searchRepos(
+        @Query("q")
+        query: String,
+        @Query("page")
+        page: Int
+    ): RepoSearchResponse
 }
