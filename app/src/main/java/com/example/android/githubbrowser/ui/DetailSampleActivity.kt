d920bec808950604c73f1ae21c5b077dbf0835be
uu082799 <ymotoba@gmail.com>
2020-02-05 09:15:38 +0900

    詳細表示用のactivity追加とdataBinding設定を有効に修正

---
+++ app/src/main/java/com/example/android/githubbrowser/ui/DetailSampleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.githubbrowser.ui
+
+import com.example.android.githubbrowser.InjectorAppCompatActivity
+import com.example.android.githubbrowser.R
+
+class DetailSampleActivity : InjectorAppCompatActivity(R.layout.activity_detail_sample) {
+    companion object {
+        const val BUNDLE_KEY_OWNER: String = "bundle_key_owner"
+        const val BUNDLE_KEY_NAME: String = "bundle_key_start_name"
+    }
+}
