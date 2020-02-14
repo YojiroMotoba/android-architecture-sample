f5d29df3fffe944f2a8217b18beb091f9bcb0e76
uu082799 <ymotoba@gmail.com>
2020-02-14 14:33:28 +0900

    ・軽微なリファクタ ・Glideで画像表示のサンプル実装

--- app/src/main/java/com/example/android/githubbrowser/binding/CustomBindingAdapter.kt
+++ app/src/main/java/com/example/android/githubbrowser/binding/CustomBindingAdapter.kt
@@ -1,9 +1,22 @@
 package com.example.android.githubbrowser.binding
 
+import android.text.TextUtils
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 @BindingAdapter("items")
 fun <T> RecyclerView.dataSetChanged(items: List<T>) {
     adapter?.notifyDataSetChanged()
+}
+
+@BindingAdapter("imageUrl")
+fun ImageView.imageURI(url: String?) {
+    if (TextUtils.isEmpty(url)) {
+        return
+    }
+    Glide.with(this)
+        .load(url)
+        .into(this)
 }
