08ff41ca38ba79238d31bec2dc78d6d46214cd66
uu082799 <ymotoba@gmail.com>
2020-02-20 13:24:15 +0900

    fragmentでdatabinding使ってみた

--- app/src/main/java/com/example/android/githubbrowser/ui/SampleFragment.kt
+++ app/src/main/java/com/example/android/githubbrowser/ui/SampleFragment.kt
@@ -1,15 +1,34 @@
 package com.example.android.githubbrowser.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.example.android.githubbrowser.InjectorFragment
 import com.example.android.githubbrowser.R
+import com.example.android.githubbrowser.databinding.FragmentSampleBinding
 import com.example.android.githubbrowser.ui.viewmodel.SampleFragmentViewModel
 
 class SampleFragment : InjectorFragment(R.layout.fragment_sample) {
 
     private val viewModel: SampleFragmentViewModel by viewModels { viewModelFactory }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentSampleBinding>(
+            inflater,
+            R.layout.fragment_sample,
+            container,
+            false
+        )
+        binding.lifecycleOwner = this
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
