99caa7e631ba925f71a76489debf1c430b1aa562
yojiromotoba <caf5a7bebd736d5a82a12724ca6c40d5f024e337>
2020-05-18 07:55:20 +0900

    ネスト深かったのでリファクタ

--- app/src/main/java/com/example/android/githubbrowser/GithubBrowserApp.kt
+++ app/src/main/java/com/example/android/githubbrowser/GithubBrowserApp.kt
@@ -1,24 +1,27 @@
 package com.example.android.githubbrowser
 
 import android.app.Application
-import com.example.android.githubbrowser.di.*
+import com.example.android.githubbrowser.di.interactorModule
+import com.example.android.githubbrowser.di.repositoryModule
+import com.example.android.githubbrowser.di.retrofitModule
+import com.example.android.githubbrowser.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class GithubBrowserApp : Application() {
 
+    private val diModules = listOf(
+        retrofitModule,
+        repositoryModule,
+        interactorModule,
+        viewModelModule
+    )
+
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@GithubBrowserApp)
-            modules(
-                listOf(
-                    retrofitModule,
-                    repositoryModule,
-                    interactorModule,
-                    viewModelModule
-                )
-            )
+            modules(diModules)
         }
     }
 }
